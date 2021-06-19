e001c1f93d6695e7359b1ae62830489f313da5aa
Edu Samblas <edusamblas@Edus-MacBook-Pro.local>
2021-06-19 20:20:49 +0200

    Added constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -3,11 +3,34 @@ pragma solidity ^0.5.0;
 import "./DappToken.sol";
 import "./DaiToken.sol";
 
+/**
+
+Deployment process:
+
+- Step 1: Deploy Dai
+- Step 2: Deploy Dapp
+- Step 3: Deploy TokenFarm
+
+ */
 contract TokenFarm {
 
     //All code goes here
 
     string public name = "Shield Token Farm";
 
+    DappToken public dappToken;
 
+    DaiToken public daiToken;
+
+    /**
+        The contract is created with the dappToken and daiToken (contract it will receive and contract it will create)
+    */
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+
+        dappToken = _dappToken;
+
+        daiToken = _daiToken;
+    }
+
+    
 } 
