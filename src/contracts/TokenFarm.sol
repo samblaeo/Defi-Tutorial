486d11d9fbc9c116bb86de86ca5643e60d422948
Edu Samblas <edusamblas@Edus-MacBook-Pro.local>
2021-06-19 20:15:24 +0200

    Imported Contracts(DappToken and DaiToken) into the TokenFarm.sol

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
 
     //All code goes here
 
     string public name = "Shield Token Farm";
 
-    
+
 } 
